36472e235ccc36be2480e06a6ec54100de98fbb8
robgion <r.giontella@2clever.it>
2022-05-04 09:33:29 +0200

    Distribuzione progetto

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fin-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'travel-master-fe';
+}

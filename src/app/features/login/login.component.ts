9fcab35467e9d1c2c4763d4014afb69ae06f2d3c
DenuccioDiego <diegodenuccio95@gmail.com>
2022-05-04 16:44:17 +0200

    CREATE routing module and login

---
+++ src/app/features/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fin-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username: string = "";
+  password: string= "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  guestAuth(username:string, password:string){
+    console.log(username);
+    
+    if(username==="Diego" && password==="Diego"){
+      //punta alla rotta del compoennte selezione mezzo / visualizza prenotazoni
+      console.log("entrato");
+      
+    }else{
+      console.log("credenziali sbagliate");
+      
+    }
+
+  }
+}
---
+++ src/app/shared/model/booking-model.ts
@@ -0,0 +1,7 @@
+export interface Booking{
+    id_viaggio: number,
+    id_prenotazione: number,
+    nome: string,
+    cognome: string,
+    posti_prenotati: number
+}

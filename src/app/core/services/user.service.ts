022e4548b41f37ba15be859e832875e401543dac
Tiziana <tiziana.tabiadon@finconsgroup.com>
2022-05-05 12:15:06 +0200

    further fixing login

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -11,7 +11,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  public getUserAuth(username: string, password: string): Observable<User> {
+  public getUserAuth(username: string, password: string): Observable<User[]> {
     const url = environment.baseUrl + '/users/';
 
     let queryParams = new HttpParams()
@@ -20,7 +20,7 @@ export class UserService {
     console.log(queryParams);
 
 
-    return this.http.get<User>(url, { params: queryParams });
+    return this.http.get<User[]>(url, { params: queryParams });
   }
 }
 
